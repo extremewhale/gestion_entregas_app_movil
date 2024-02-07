8842a000ffb2d9db4424edf948604b0a326b3d94
Carlos Alberto Antonio Almerco <carbertonio@gmail.com>
2024-02-06 20:19:50 -0500

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Delivery',
+      title: 'HOLA',
       initialRoute: AppRoutes.LOGIN,
       getPages: AppPages.pages,
     );
